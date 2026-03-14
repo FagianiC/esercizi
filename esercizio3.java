3e137105f3b15fb3d7cd1fd4d04c51a2ea9d39c4
FagianiC <fagiani.cristian.studente@itispaleocapa.it>
2026-03-14 12:46:16 +0100

    esercizio3

---
+++ esercizio3.java
@@ -0,0 +1,22 @@
+package esercizi;
+
+import java.util.Scanner;
+
+public class esercizio3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Inserisci il tuo nome: ");
+        String nome = scanner.nextLine();
+
+     
+        System.out.print("Inserisci la tua età: ");
+        int eta = scanner.nextInt();
+
+        System.out.println("Ciao " + nome + ", hai " + eta + " anni");
+
+        scanner.close();
+    }
+}
