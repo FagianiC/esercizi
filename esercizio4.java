45325f5776d59a37cfcbf57bcceed2ebc7c942c5
FagianiC <fagiani.cristian.studente@itispaleocapa.it>
2026-03-14 12:48:32 +0100

    esercizio4

---
+++ esercizio4.java
@@ -0,0 +1,14 @@
+package esercizi;
+
+public class esercizio4 {
+
+   
+    public static int somma(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int risultato = somma(5, 3); 
+        System.out.println("La somma è: " + risultato);
+    }
+}
